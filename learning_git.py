35be80d78a47ffad9a7e8a6b596b8d9ee0f0e9a2
Amber Jain <amberj@chem.iitb.ac.in>
2020-03-03 14:29:29 +0530

    added function for addition

--- learning_git.py
+++ learning_git.py
@@ -1,7 +1,13 @@
+def addition(a,b):
+  c=a+b
+  return(c)
+
 print("Hello World")
 a=1.0
 b=2.0
 print(a+b)
+print("adding using function")
+print(addition(a,b))
 
 c=a*b
 print("This version has added more changes")
